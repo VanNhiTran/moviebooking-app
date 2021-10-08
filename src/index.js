500526e5c38362cd0fa534e8c6f43b5bec6431af
Nguyen Dinh Thinh <thinhnguyendn93@gmail.com>
2021-10-08 14:44:11 +0700

    added tailwind build

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { Provider } from "react-redux";
 import { store } from "./redux/configStore";
+
+import "./index.css";
 
 //setup ant design
 import "antd/dist/antd.css";
